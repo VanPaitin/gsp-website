7289ce1e37b9d05f5e23f34a76466391765254ee
Mayowa Pitan <mayowa.pitan@andela.com>
2018-10-21 09:30:34 +0100

    update amazon credentials

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,8 @@ module GspWebsite
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    # config.middleware.insert_after ActiveRecord::Migration::CheckPending,  ActionDispatch::Session::CookieStore
+    # config.middleware.insert_after ActionDispatch::Cookies, ActionDispatch::Session::CookieStore
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
